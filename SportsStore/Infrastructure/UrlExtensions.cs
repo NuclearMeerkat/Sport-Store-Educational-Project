fa199c4a21befe0719fdc6f36d8482952fa41e8f
Maksym <maxympuliak@gmail.com>
2024-10-24 22:51:13 +0300

    Initial commit

--- SportsStore/Infrastructure/UrlExtensions.cs
+++ SportsStore/Infrastructure/UrlExtensions.cs
@@ -4,9 +4,7 @@
     {
         public static string PathAndQuery(this HttpRequest request)
         {
-#pragma warning disable CA1062 // Validate arguments of public methods
             return request.QueryString.HasValue ? $"{request.Path}{request.QueryString}" : request.Path.ToString();
-#pragma warning restore CA1062 // Validate arguments of public methods
         }
     }
 }
