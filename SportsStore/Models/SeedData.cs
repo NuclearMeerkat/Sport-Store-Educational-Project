3a6ef7f3d4e8e7e1d30e684edea3b67d9c7fadac
Maksym <maxympuliak@gmail.com>
2024-11-25 19:20:52 +0200

    PictureUrl field added to Product model

--- SportsStore/Models/SeedData.cs
+++ SportsStore/Models/SeedData.cs
@@ -23,6 +23,7 @@ namespace SportsStore.Models
                         Description = "A boat for one person",
                         Category = "Watersports",
                         Price = 275,
+                        PictureUrl = "https://w7.pngwing.com/pngs/420/270/png-transparent-boat-sea-kayak-recreational-kayak-watercraft-paddle-sports-equipment-vehicle-sports-thumbnail.png",
                     },
                     new Product
                     {
@@ -30,6 +31,7 @@ namespace SportsStore.Models
                         Description = "Protective and fashionable",
                         Category = "Watersports",
                         Price = 48.95m,
+                        PictureUrl = "https://w7.pngwing.com/pngs/646/324/png-transparent-gilets-life-jackets-sport-livery-life-jacket-sport-livery-life-jacket-thumbnail.png",
                     },
                     new Product
                     {
@@ -37,6 +39,7 @@ namespace SportsStore.Models
                         Description = "FIFA-approved size and weight",
                         Category = "Soccer",
                         Price = 19.50m,
+                        PictureUrl = "https://img.freepik.com/premium-psd/realistic-soccer-ball-png_679658-11799.jpg",
                     },
                     new Product
                     {
@@ -44,6 +47,7 @@ namespace SportsStore.Models
                         Description = "Give your playing field a professional touch",
                         Category = "Soccer",
                         Price = 34.95m,
+                        PictureUrl = "https://e7.pngegg.com/pngimages/465/128/png-clipart-flag-of-the-united-states-corner-flag-flag-triangle.png",
                     },
                     new Product
                     {
@@ -51,6 +55,7 @@ namespace SportsStore.Models
                         Description = "Flat-packed 35,000-seat stadium",
                         Category = "Soccer",
                         Price = 79500,
+                        PictureUrl = "https://img.freepik.com/free-psd/beautiful-3d-isometric-stadium-isolated-transparent-background_191095-9012.jpg",
                     },
                     new Product
                     {
@@ -58,6 +63,7 @@ namespace SportsStore.Models
                         Description = "Improve brain efficiency by 75%",
                         Category = "Chess",
                         Price = 16,
+                        PictureUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQxExGOdWnDouBJ9qVp2jC2b3SSRfajFtClw&s",
                     },
                     new Product
                     {
@@ -65,6 +71,7 @@ namespace SportsStore.Models
                         Description = "Secretly give your opponent a disadvantage",
                         Category = "Chess",
                         Price = 29.95m,
+                        PictureUrl = "https://png.pngtree.com/png-vector/20241109/ourlarge/pngtree-sturdy-wooden-chair-with-natural-finish-png-image_14339638.png",
                     },
                     new Product
                     {
@@ -72,6 +79,7 @@ namespace SportsStore.Models
                         Description = "A fun game for the family",
                         Category = "Chess",
                         Price = 75,
+                        PictureUrl = "https://e7.pngegg.com/pngimages/827/531/png-clipart-chessboard-staunton-chess-set-chess-piece-szachy-king-board-game.png",
                     },
                     new Product
                     {
@@ -79,6 +87,7 @@ namespace SportsStore.Models
                         Description = "Gold-plated, diamond-studded King",
                         Category = "Chess",
                         Price = 1200,
+                        PictureUrl = "https://w7.pngwing.com/pngs/92/270/png-transparent-gold-chess-piece-chess-kids-xiangqi-king-chess-king-chess-game-sports.png",
                     });
 
                 context.SaveChanges();
